601e63e08d63074e05d7337d58f61fb51925cc76
annis <nissinkentang@gmail.com>
2022-01-03 23:36:45 +0700

    Add project files.

---
+++ ServerConfigRest_20190140037_AnnisaRisyafa/Program.cs
@@ -0,0 +1,34 @@
+﻿using ServiceRest_20190140037_AnnisaRisyafa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ServerConfigRest_20190140037_AnnisaRisyafa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server ready");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
